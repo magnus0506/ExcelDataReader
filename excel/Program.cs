883f62714c9eb40d3755686b5530dd33c06b9919
Magnus Reidl Allerup <magnusreidl@gmail.com>
2022-04-08 10:44:39 +0200

    lol

--- excel/Program.cs
+++ excel/Program.cs
@@ -9,9 +9,17 @@ namespace excel
     {
         static void Main(string[] args)
         {
+            //TimeValueArr();
+            ISO17665();
+        }
+
+        public static void TimeValueArr()
+        {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            var arr = new List<TimeValue>();
-            string filePath = "/Users/magnusallerup/Documents/names.xlsx";
+            var timeValueArr = new List<TimeValue>();
+            var timeArr = new List<String>();
+            var valueArr = new List<Double>();
+            string filePath = "C:/Users/mra/Documents/wery7232XH.xlsx";
 
             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
             {
@@ -20,32 +28,68 @@ namespace excel
                 {
                     while (reader.Read())
                     {
-                        var resTime = reader.GetValue(0);
-                        var resValue = reader.GetValue(1);
+                        var time = reader.GetValue(0);
+                        var timeType = reader.GetFieldType(0);
+                        var value = reader.GetValue(2);
+                        var valueType = reader.GetFieldType(2);
 
-                        if (resTime is double)
+                        if (time is string && value is double)
                         {
-                            arr.Add(new TimeValue((double)resTime, (double)resValue));
+                            timeArr.Add((string)time);
+                            timeValueArr.Add(new TimeValue((string)time, (double)value));
                         }
-
-
                     }
                 } while (reader.NextResult());
             }
+            //timeArr.ForEach(x => Console.WriteLine(x));
+            //timeValueArr.ForEach(x => Console.WriteLine($"Time: {x.time}, Value: {x.value}"));
 
-            arr.ForEach(x => Console.WriteLine($"Time: {x.time}, Value: {x.value}"));
+            Console.Read();
+        }
+        public static void ISO17665()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            string filePath = "C:/Users/mra/Documents/test.xlsx";
 
-            var a = arr.Average(x => x.value);
-            Console.WriteLine($"Average value: {a}");
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using var reader = ExcelReaderFactory.CreateReader(stream);
+                do
+                {
+                    while (reader.Read())
+                    {
+                        object col1 = reader.GetValue(0);
+                        var col1Type = reader.GetFieldType(0);
+                        
+                        object col3 = reader.GetValue(2);
+                        var col3Type = reader.GetFieldType(0);
+                        
+                        object col5 = reader.GetValue(4);
+                        var col5Type = reader.GetFieldType(0);
+
+                        var row = reader.Depth;
+
+                        PrintCol(row, col5Type, col5);
+                    }
+                } while (reader.NextResult());
+            }
             Console.Read();
         }
 
+        public static void PrintCol(int col, Type type, object value)
+        {
+            if (value != null)
+            {
+                Console.WriteLine($"Row {col + 1}: {type}: {value}");
+            }
+        }
+        
         public class TimeValue
         {
-            public double time { get; set; }
+            public string time { get; set; }
             public double value { get; set; }
 
-            public TimeValue(double x, double y)
+            public TimeValue(string x, double y)
             {
                 time = x;
                 value = y;
